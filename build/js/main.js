4698bc78486c0d88e3b7cbc0e56383b981d45b66
Alex <87208475+PuSh0chek@users.noreply.github.com>
2023-05-15 16:55:52 +0300

    0.0012

--- build/js/main.js
+++ build/js/main.js
@@ -1,1 +1,5 @@
 "use strict";
+
+var _swiper = _interopRequireDefault(require("swiper"));
+require("swiper/css");
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
