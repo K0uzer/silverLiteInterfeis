5a65e8cd4bf592821f3281567fb3998fecf358ac
Alex <87208475+PuSh0chek@users.noreply.github.com>
2023-05-16 18:09:00 +0300

    Доделал окно для работы с папками

--- build/js/main.js
+++ build/js/main.js
@@ -1,5 +1,14 @@
 "use strict";
 
-var _swiper = _interopRequireDefault(require("swiper"));
-require("swiper/css");
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+// ПЕРЕМЕННЫЕ //
+const buttonOfOpenFolderWindow = document.querySelector('.archive__filder-menu-img');
+const buttonOfCloseFolderWindow = document.querySelector('.archive__filder-menu-img--rotate');
+const windowThisFolder = document.querySelector('.archive__folder');
+
+// СЛУШАТЕЛИ СОБЫТИЫЙ ДЛЯ РАБОТЫ С ОКНОМ ПАПОК //
+buttonOfOpenFolderWindow.addEventListener('click', () => {
+  windowThisFolder.style = 'display: block';
+});
+buttonOfCloseFolderWindow.addEventListener('click', () => {
+  windowThisFolder.style = 'display: none';
+});
--- source/js/main.js
+++ source/js/main.js
@@ -1,3 +1,13 @@
-import Swiper from 'swiper';
-// import Swiper styles
-import 'swiper/css';
+// ПЕРЕМЕННЫЕ //
+const buttonOfOpenFolderWindow = document.querySelector('.archive__filder-menu-img');
+const buttonOfCloseFolderWindow = document.querySelector('.archive__filder-menu-img--rotate');
+const windowThisFolder = document.querySelector('.archive__folder');
+
+// СЛУШАТЕЛИ СОБЫТИЫЙ ДЛЯ РАБОТЫ С ОКНОМ ПАПОК //
+buttonOfOpenFolderWindow.addEventListener('click', () => {
+  windowThisFolder.style = 'display: block';
+});
+
+buttonOfCloseFolderWindow.addEventListener('click', () => {
+  windowThisFolder.style = 'display: none';
+});
