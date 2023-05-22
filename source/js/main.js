9d08210b72bff3d2fbac03ece13cde2dd2974a89
Alex <87208475+PuSh0chek@users.noreply.github.com>
2023-05-22 16:33:56 +0300

    наложит стили и анимацию на окно поиска, рассортирует стили по отдельным докам

--- source/js/main.js
+++ source/js/main.js
@@ -2,6 +2,8 @@
 const buttonOfOpenFolderWindow = document.querySelector('.archive__filder-menu-img');
 const buttonOfCloseFolderWindow = document.querySelector('.archive__filder-menu-img--rotate');
 const windowThisFolder = document.querySelector('.archive__folder');
+const searchOfDateRegButton = document.getElementById('searchOfDateReg');
+const archiveContainerFileSearchOfElementWrapper = document.querySelector('.archive__container-file-search-of-element-wrapper');
 
 // СЛУШАТЕЛИ СОБЫТИЫЙ ДЛЯ РАБОТЫ С ОКНОМ ПАПОК //
 buttonOfOpenFolderWindow.addEventListener('click', () => {
@@ -11,3 +13,24 @@ buttonOfOpenFolderWindow.addEventListener('click', () => {
 buttonOfCloseFolderWindow.addEventListener('click', () => {
   windowThisFolder.style = 'display: none';
 });
+
+// ОКНО ПОИСКА //
+searchOfDateRegButton.addEventListener('click', () => {
+  archiveContainerFileSearchOfElementWrapper.classList.add('archive__container-file-search-of-element-wrapper--active');
+  searchOfDateRegButton.style = 'display: none';
+});
+
+// БЕЗОПАСНОСТЬ //
+document.addEventListener('keydown', (event) => {
+  if (event.shiftKey && event.ctrlKey && event.key === 'c') {
+    console.log('You pressed Shift + Ctrl + C!');
+  }
+  console.log('You pressed Shift');
+});
+
+document.addEventListener('contextmenu', (e) => {
+  console.log('listener of context menu');
+  window.location.href = 'http://localhost:3000/authorization.html';
+  e.preventDefalt();
+});
+
